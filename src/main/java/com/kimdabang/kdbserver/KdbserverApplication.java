70e415d2d64e79a7faedde68fb889135f51dbafb
PSangEon <qkr99102@naver.com>
2024-08-26 16:00:56 +0900

    초기 커밋

---
+++ src/main/java/com/kimdabang/kdbserver/KdbserverApplication.java
@@ -0,0 +1,13 @@
+package com.kimdabang.kdbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdbserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdbserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimdabang/kdbserver/KdbserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimdabang.kdbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdbserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
