39cc3bea5906fe213dbd510b88603c730f3ec143
niyamallo <treenicole@naver.com>
2024-09-02 15:43:19 +0900

    [FEAT] 상품 미디어 API 추가, 파라미터 수정

---
+++ src/main/java/com/kimdabang/kdbserver/product/media/presentation/ProductMediaController.java
@@ -0,0 +1,99 @@
+package com.kimdabang.kdbserver.product.media.presentation;
+
+import com.kimdabang.kdbserver.common.entity.CommonResponseEntity;
+import com.kimdabang.kdbserver.product.media.application.ProductMediaService;
+import com.kimdabang.kdbserver.product.media.dto.in.ProductMediaRequestDto;
+import com.kimdabang.kdbserver.product.media.dto.out.ProductMediaResponseDto;
+import com.kimdabang.kdbserver.product.media.vo.ProductMediaRequestVo;
+import com.kimdabang.kdbserver.product.media.vo.ProductMediaResponseVo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/product-media")
+public class ProductMediaController {
+
+    private final ProductMediaService productMediaService;
+
+    @PostMapping
+    public CommonResponseEntity<Void> createProductMedia(
+            @RequestBody ProductMediaRequestVo productMediaRequestVo) {
+        log.info("productMediaRequestVo: {}", productMediaRequestVo);
+        ProductMediaRequestDto productMediaRequestDto = ProductMediaRequestDto.builder()
+                .productId(productMediaRequestVo.getProductId())
+                .mediaName(productMediaRequestVo.getMediaName())
+                .mediaType(productMediaRequestVo.getMediaType())
+                .mediaURL(productMediaRequestVo.getMediaURL())
+                .build();
+        productMediaService.addProductMedia(productMediaRequestDto);
+        return new CommonResponseEntity<>(
+                HttpStatus.OK,
+                "상품 미디어 등록 성공",
+                null
+        );
+    }
+
+    @PutMapping
+    public CommonResponseEntity<Void> updateProductMedia(
+            @RequestBody ProductMediaRequestVo productMediaRequestVo) {
+        log.info("productMediaRequestVo: {}", productMediaRequestVo);
+        ProductMediaRequestDto productMediaRequestDto = ProductMediaRequestDto.builder()
+                .productId(productMediaRequestVo.getProductId())
+                .mediaName(productMediaRequestVo.getMediaName())
+                .mediaType(productMediaRequestVo.getMediaType())
+                .mediaURL(productMediaRequestVo.getMediaURL())
+                .build();
+        productMediaService.updateProductMedia(productMediaRequestDto);
+        return new CommonResponseEntity<>(
+                HttpStatus.OK,
+                "상품 미디어 업데이트 성공",
+                null
+        );
+    }
+
+    @DeleteMapping
+    public CommonResponseEntity<Void> deleteProductMedia(
+            @RequestBody String productMediaId) {
+        log.info("productMediaId: {}", productMediaId);
+        return new CommonResponseEntity<>(
+                HttpStatus.OK,
+                "상품 미디어 삭제 성공",
+                null
+        );
+    }
+
+    @GetMapping("/{productMediaId}")
+    public CommonResponseEntity<ProductMediaResponseVo> getProductMedia(
+            @PathVariable String productMediaId) {
+        ProductMediaResponseDto productMediaResponseDto = productMediaService.getProductMedia(productMediaId);
+
+        return new CommonResponseEntity<>(
+                HttpStatus.OK,
+                "상품 미디어 조회 성공",
+                productMediaResponseDto.toProductMediaResponseVo()
+        );
+    }
+
+    @GetMapping("{productId}/all")
+    public CommonResponseEntity<List<ProductMediaResponseVo>> getAllProductMedia(
+            @PathVariable String productId) {
+        List<ProductMediaResponseDto> productMediaResponseDtoList = productMediaService.getAllProductMedia(productId);
+
+        List<ProductMediaResponseVo> productMediaResponseVoList
+                = productMediaResponseDtoList.stream()
+                .map(ProductMediaResponseDto::toProductMediaResponseVo)
+                .toList();
+
+        return new CommonResponseEntity<>(
+                HttpStatus.OK,
+                "상품 미디어 리스트 조회 성공",
+                productMediaResponseVoList
+        );
+    }
+}
