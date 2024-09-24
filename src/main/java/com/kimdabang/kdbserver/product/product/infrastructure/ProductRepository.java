9c4a8d8a34f6f2b81efbdee0cb2b4ef703ddf902
niyamallo <treenicole@naver.com>
2024-09-24 10:07:45 +0900

    Feat: 상품 페이지네이션 Repository 생성

--- src/main/java/com/kimdabang/kdbserver/product/product/infrastructure/ProductRepository.java
+++ src/main/java/com/kimdabang/kdbserver/product/product/infrastructure/ProductRepository.java
@@ -2,8 +2,12 @@ package com.kimdabang.kdbserver.product.product.infrastructure;
 
 import com.kimdabang.kdbserver.product.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +15,14 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findById(Long productId);
     Optional<Product> findByProductCode(String productCode);
+
+    @Query(value = "SELECT p FROM Product p WHERE p.categoryId = :categoryId ORDER BY p.id")
+    List<Product> findProductsByCategoryWithPagination(
+            @Param("categoryId") Long categoryId,
+            @Param("offset") int offset,
+            @Param("size") int size
+    );
+
+    @Query(value = "SELECT COUNT(p) FROM Product p WHERE p.categoryId = :categoryId")
+    Long countProductsByCategory(@Param("categoryId") Long categoryId);
 }
