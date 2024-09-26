87804ba19c9138cd8ca9efd2e83d983f5b13e8c5
niyamallo <treenicole@naver.com>
2024-09-26 13:51:11 +0900

    Fix: 엘라스틱서치 명시적 어노테이션 추가

--- src/main/java/com/kimdabang/kdbserver/product/product/infrastructure/ProductSearchRepository.java
+++ src/main/java/com/kimdabang/kdbserver/product/product/infrastructure/ProductSearchRepository.java
@@ -3,9 +3,11 @@ package com.kimdabang.kdbserver.product.product.infrastructure;
 import com.kimdabang.kdbserver.product.product.domain.ProductDocument;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 import java.util.List;
 
+@EnableElasticsearchRepositories
 public interface ProductSearchRepository extends ElasticsearchRepository<ProductDocument, String> {
 
     List<ProductDocument> findByProductNameContainingOrDescriptionContaining(String name, String description, Pageable pageable);
