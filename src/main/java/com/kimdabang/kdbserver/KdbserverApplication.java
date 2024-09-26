79118bab63b2f6a3ed9da75a507b4d47118a570d
niyamallo <treenicole@naver.com>
2024-09-26 14:33:17 +0900

    Fix: 엘라스틱서치 import 수정

--- src/main/java/com/kimdabang/kdbserver/KdbserverApplication.java
+++ src/main/java/com/kimdabang/kdbserver/KdbserverApplication.java
@@ -1,7 +1,9 @@
 package com.kimdabang.kdbserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class KdbserverApplication {
